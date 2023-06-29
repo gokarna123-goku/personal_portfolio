b798000919477f381f13e4b80c0c8ba167572165
gokarna123-goku <agokarna444@gmail.com>
2023-06-29 13:23:22 +0545

    added js file

--- main.js
+++ main.js
@@ -10,4 +10,25 @@ window.addEventListener("scroll", function(){
         navbar.classList.remove("fixed");
         navbar.style.transition = "0.5s ease-out";
     }
-})
+});
+
+
+// Toggle Light and Dark Mode
+function changeMode() {
+    var body = document.querySelector(".main_section");
+    var toggleIcon = document.querySelector("#toggle-icon");
+
+    body.classList.toggle("light-mode");
+
+    if (body.classList.contains("light-mode")) {
+        toggleIcon.style.backgroundImage = "url('./img/mode/moon.png')";
+        toggleIcon.style.transition = "0.3s ease-out";
+    } else {
+        toggleIcon.style.backgroundImage = "url('./img/mode/sun.png')";
+        toggleIcon.style.transition = "0.3s ease-out";
+    }
+}
+  
+
+
+// END
